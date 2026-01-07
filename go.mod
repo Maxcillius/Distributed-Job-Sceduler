12da1c7557ab2bd223e9ea542dbc8b2a340ee3cd
maxcillius <alphaviperchamber@hotmail.com>
2026-01-08 00:00:36 +0530

    Finilized watcher implementation

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/maxcillius/Distributed-Job-Scheduler
 
 go 1.24.4
 
-require gopkg.in/yaml.v3 v3.0.1 // indirect
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	golang.org/x/sys v0.39.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- pkg/watcher.go
+++ pkg/watcher.go
@@ -4,32 +4,72 @@ import (
 	"context"
 	"fmt"
 	"os/exec"
+	"strings"
 	"time"
 )
 
-func Watcher(ctx context.Context, errChan chan<- error) {
+type head struct {
+	commit string
+}
+
+// func getPath(key string) (string, bool) {
+// 	value, ok := os.LookupEnv(key)
+// 	if !ok {
+// 		return "", false
+// 	}
+// 	return value, true
+// }
+
+func Watcher(ctx context.Context, trigChan chan<- struct{}, errChan chan<- error) {
+	currCommit := &head{
+		"",
+	}
+	// directory, ok := getPath("DIR")
+	// fmt.Println(directory)
+	// if !ok {
+	// 	err := fmt.Errorf("No jobs directory found\n")
+	// 	errChan <- err
+	// 	return
+	// }
+	gitPath, err := exec.Command("which", "git").Output()
+	if err != nil {
+		errChan <- err
+		return
+	}
 	for {
-		pull := exec.Cmd{
-			Path: "/usr/bin/git",
-			Args: []string{"git", "pull"},
-			Dir:  "./Jobs",
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			pull := exec.Cmd{
+				Path: strings.TrimSpace(string(gitPath)),
+				Args: []string{"git", "pull"},
+				Dir:  "./Jobs",
+			}
+			if err := pull.Run(); err != nil {
+				errChan <- err
+				return
+			}
+			fmt.Println("Refreshed the repo")
+			fmt.Println("Checking for udpate...")
+			checkHead := exec.Cmd{
+				Path: strings.TrimSpace(string(gitPath)),
+				Args: []string{"git", "rev-parse", "HEAD"},
+				Dir:  "./Jobs",
+			}
+			info, err := checkHead.Output()
+			if err != nil {
+				errChan <- err
+				fmt.Printf("Error while checking HEAD: %v\n", err)
+				return
+			}
+			newCommit := string(info)
+			if currCommit.commit != newCommit {
+				currCommit.commit = newCommit
+				trigChan <- struct{}{}
+			}
+			time.Sleep(time.Minute)
+			// Add ticker instead
 		}
-		if err := pull.Run(); err != nil {
-			errChan <- err
-			fmt.Printf("%v\n", err)
-			panic("cannot pull repo")
-		}
-		fmt.Println("Refreshed the repo")
-		fmt.Println("Checking for udpate...")
-		checkHead := exec.Cmd{
-			Path: "usr/bin/git",
-			Args: []string{"git", "show", "HEAD"},
-			Dir:  "./Jobs",
-		}
-		if err := checkHead.Run(); err != nil {
-			errChan <- err
-			fmt.Printf("Error while checking HEAD: %v", err)
-		}
-		time.Sleep(time.Minute)
 	}
 }
